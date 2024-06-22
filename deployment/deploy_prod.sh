7fc24dd85d9142e8e44c5271c2c186ebc4195f88
Nishesh Patel <jeepraofficial@gmail.com>
2024-06-22 17:21:14 +0545

    Initial commit

---
+++ deployment/deploy_prod.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ssh root@161.35.110.213 <<EOF
+  cd myblog
+  git pull 
+  source env/bin/activate
+  ./manage.py migrate
+  sudo systemctl restart nginx
+  sudo service gunicorn restart
+  sudo service nginx restart
+  exit
+EOF
